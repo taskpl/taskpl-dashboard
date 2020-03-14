f43ba9f4bf62f836ad007651c8760ac462da9c2b
williamfzc <178894043@qq.com>
2020-03-14 15:49:59 +0800

    feat: move from element-starter

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-aside>
+        <sideBar/>
+      </el-aside>
+
+      <el-container>
+        <el-header>
+          <topBar/>
+        </el-header>
+
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import TopBar from './components/TopBar.vue'
+import SideBar from './components/SideBar.vue'
+
+export default {
+  components: {
+    topBar: TopBar,
+    sideBar: SideBar,
+  }
+}
+</script>
+
